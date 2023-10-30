4142826760a14e041eb4059e70747c3d7f13aef8
Kallen-Hsu <kallen.hsu82@gmail.com>
2023-10-30 13:49:11 +0800

    update

---
+++ app/src/main/java/com/example/bodyboost/sport/SportItems.kt
@@ -0,0 +1,21 @@
+package com.example.bodyboost.sport
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.bodyboost.R
+
+class SportItems: AppCompatActivity()  {
+    private lateinit var animate: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sport_info)
+
+
+        animate = findViewById(R.id.animate)
+        Glide.with(animate).load("https://storage.googleapis.com/bodyboost-bucket/animation_video/Lisa-elbow-knee-touch2-light-unscreen.gif").into(animate)
+
+    }
+
+}
